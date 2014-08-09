62d7d1a85a963a8035c19241d4fd4b0ee6d63abe
unknown <chrissy.ongy@gmail.com>
2014-08-09 14:52:02 -0700

    coe-seatwork-four

--- sw3.js
+++ sw3.js
@@ -1,11 +1,4 @@
-function countChar(a,b){
-	var c = a.length,d = 0;
-	for(var e = 0; e < c; e++){
-		if( a.substring(e, e+1) == b){d++;};
-	}
-	return d;
-}
-console.log(countChar("kakkerlak", "k"));
+
 function min(a,b){
 	return a < b ? a : b; 
 }
