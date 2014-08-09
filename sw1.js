b860a783b077738fd71bcd3b5da182acc811b1ee
unknown <chrissy.ongy@gmail.com>
2014-08-09 14:39:00 -0700

    coe-seatwork-four

--- sw1.js
+++ sw1.js
@@ -1,3 +1,9 @@
-for (i=1; i<=7; i=i+1) {
-	console.log("#");
+function drawTriangle(x){
+	var y = "#", z = "";
+	for(var j = 1; j<=x; j++){
+		for(var i = x; i<=x; i++ ){
+			z += y;
+		}
+		console.log(z);
+	}
 }
